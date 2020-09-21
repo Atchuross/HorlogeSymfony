b0a8c80eebdeab942399623ac9d70f67aea5e58e
Atchuross <c.clauzel@icloud.com>
2020-09-21 16:28:07 +0200

    Worked version

---
+++ var/cache/dev/twig/1d/1d137a70385786d8012c12711449db66500ccdf05c2b5ac0d67583a731a5263b.php
@@ -0,0 +1,157 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* home.html.twig */
+class __TwigTemplate_2257c83c39f61f303b70dc2aa8b647fac6ca9064b82857541413313c41267d21 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'stylesheets' => [$this, 'block_stylesheets'],
+            'title' => [$this, 'block_title'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "home.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "home.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 3
+    public function block_stylesheets($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        // line 4
+        echo "    <link rel=\"stylesheet\" href=\"/css/style.css\">
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 7
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Horloge France | ";
+        $this->displayParentBlock("title", $context, $blocks);
+        echo " ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 9
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 10
+        echo "<div class=\"flex-center full-height\">
+    <div class=\"content\">
+        <img class=\"flag\" src=\"/img/flag.svg\" alt=\"\">
+        <h1 class=\"title\">Hello from France</h1>
+
+
+        <p>🕥 Il est actuellement ";
+        // line 16
+        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, "now", "H:i", "Europe/Paris"), "html", null, true);
+        echo " en France.</p>
+
+        <hr>
+
+        <p>&copy; Copyright 2020 - ";
+        // line 20
+        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, "now", "Y", "Europe/Paris"), "html", null, true);
+        echo "</p>
+    </div>
+</div>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "home.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  106 => 20,  99 => 16,  91 => 10,  84 => 9,  69 => 7,  61 => 4,  54 => 3,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block stylesheets %}
+    <link rel=\"stylesheet\" href=\"/css/style.css\">
+{% endblock %}
+
+{% block title %}Horloge France | {{ parent() }} {% endblock title %}
+
+{% block body %}
+<div class=\"flex-center full-height\">
+    <div class=\"content\">
+        <img class=\"flag\" src=\"/img/flag.svg\" alt=\"\">
+        <h1 class=\"title\">Hello from France</h1>
+
+
+        <p>🕥 Il est actuellement {{'now'|date('H:i', 'Europe/Paris')}} en France.</p>
+
+        <hr>
+
+        <p>&copy; Copyright 2020 - {{'now'|date('Y', 'Europe/Paris')}}</p>
+    </div>
+</div>
+{% endblock body %}", "home.html.twig", "/Users/clement/Desktop/sym/hello-app/templates/home.html.twig");
+    }
+}
